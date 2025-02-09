fb5c9d104b9f33ada6a564b0b540e2a58660691f
Batanayi Matuku <batanayi.matuku@mtn.com>
2025-02-09 22:20:06 +0200

    Added new theme assets

--- components/Action/Action.tsx
+++ components/Action/Action.tsx
@@ -1,14 +1,16 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from './Action.module.scss'
-import { ReactNode } from 'react'
+import React from 'react';
+import Image from 'next/image';
+import styles from './Action.module.scss';
+import clsx from 'clsx';
 
 interface ActionProps {
-  theme?: string
-  accent?: string
-  image?: string
-  title: ReactNode
-  subtitle: ReactNode
+  theme?: 'purple' | 'pink' | 'green' | 'yellow';
+  accent?: 'darkPink' | 'darkPurple' | 'darkGreen' | 'darkYellow';
+  image?: string;
+  title?: React.ReactNode;
+  subtitle?: string;
+  buttonText?: string;
+  buttonLink?: string;
 }
 
 export function Action({ 
@@ -16,35 +18,33 @@ export function Action({
   accent = "darkPink",
   image = "/icons/GD-Icon-009.png",
   title = <><span className="text-yellow">Delight</span> your customer</>,
-  subtitle = "If you were to ask us, what's it like to launch a successful brand? Our answer is simple: It's always a delight working with upcoming brands making their dreams a reality."
+  subtitle = "If you were to ask us, what's it like to launch a successful brand? Our answer is simple: It's always a delight working with upcoming brands making their dreams a reality.",
+  buttonText = "Get started",
+  buttonLink = "/#contact-us"
 }: ActionProps) {
   return (
-    <section className={`${styles.action} ${styles[theme]}`}>
+    <section className={styles.action}>
       <div className={styles.actionContent}>
-        <div className={`${styles.actionCard} ${styles[accent]}`}>
+        <div className={styles.actionCard}>
           <div className={styles.content}>
-            <div className={styles.text}>
-              <h2>{title}</h2>
-              <p>{subtitle}</p>
-              <Link href="/#contact-us">
-                <button className={styles.button}>
-                  Get started
-                </button>
-              </Link>
-            </div>
-            <div className={styles.image}>
-              <div className={`${styles.imageWrapper} ${styles[accent]}`}>
-                <Image
-                  src={image}
-                  alt="Action Image"
-                  width={1200}
-                  height={1200}
-                />
-              </div>
+            <h2>{title}</h2>
+            <p>{subtitle}</p>
+            <a href={buttonLink}>
+              <button className={styles.button}>{buttonText}</button>
+            </a>
+          </div>
+          <div className={styles.image}>
+            <div className={clsx(styles.imageWrapper, styles[theme])}>
+              <Image 
+                src={image} 
+                alt="Action Image"
+                width={1200}
+                height={1200}
+              />
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
